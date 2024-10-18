51a9cd6ab65167704e6487fac70e25513b1726af
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-10-18 01:16:15 -0400

    Finished Lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
-
+    with open(f'{file_name}.txt', "w", encoding='utf-8') as file:
+        file.write(file_content)
+   
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', "a", encoding='utf-8') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', "r") as file:
+        return file.read()
+
